7bd6ad61a21cd16a59d580d640d7252e28f7463d
doctor <doctor@trev.xyz>
2026-09-25 22:03:19 +0000

    bump: v0.6.2 -> v0.7.0

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
           default = pkgs.stdenv.mkDerivation (
             final: with pkgs.lib; {
               pname = "gleam-template";
-              version = "0.6.2";
+              version = "0.7.0";
 
               src = fileset.toSource {
                 root = ./.;
